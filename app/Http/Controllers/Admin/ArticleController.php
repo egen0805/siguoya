e677f9844909f75a95a228f8fa3c53317505122a
mypc <924714558@qq.com>
2015-11-28 18:48:22 +0800

    重新上传更改了大小写的Controller文件夹

---
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Article;
+use App\ArticleClassify;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Common\Controller;
+
+class ArticleController extends Controller
+{
+    public function index(){
+	    $articleList=Article::all();
+		return view('admin.article.index',compact('articleList'));
+    }
+
+	public function show(){
+
+	}
+
+	public function create(){
+		$classifyList=$this->getClassifyList(0);
+		return view('admin.article.create',compact('classifyList'));
+	}
+
+	public function store(){
+		echo Article::create(\Request::all());
+	}
+
+	public function edit($id){
+		$article=Article::find($id);
+		$classifyList=$this->getClassifyList(0);
+		return view('admin.article.edit',compact('article','classifyList'));
+	}
+
+	public function update(){
+		$article=Article::find(\Request::input('article_id'));
+		echo $article->update(\Request::all());
+	}
+
+	public function destroy(){
+
+	}
+
+
+	public function ajaxClassifyList(){
+		if(\Request::Ajax()){
+			echo json_encode($this->getClassifyList(\Request::input('mainId')));
+			exit();
+		}
+	}
+
+
+	/**
+	 * 获取文章分类列表
+	 * @param $pid 父分类ID
+	 * @return mixed
+	 */
+	public function getClassifyList($pid){
+		$classifyList=ArticleClassify::where('classify_parent_id','=',$pid)
+		                             ->lists('classify_name','classify_id')
+		                             ->toArray();
+		return $classifyList;
+	}
+
+
+}
