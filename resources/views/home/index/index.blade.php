568f253a160bf7bc33cf579e044bbf6d83ccbb2c
mypc <924714558@qq.com>
2016-02-16 18:12:14 +0800

    首页内容更新

--- resources/views/home/index/index.blade.php
+++ resources/views/home/index/index.blade.php
@@ -1,55 +1,76 @@
 @extends('home.common.layout')
 @section('main')
-    <div id="new_list" class="col-sm-3 col-xs-12">
-        <div id="append_new" class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title">最新文章</h3>
-            </div>
-            @foreach($articlesNew as $article)
-                <div class="panel-body padding-none">
-                    <ul class="list-unstyled">
-                        <li>
-                            <a href="{{'/home/article/'.$article->article_id}}">
-                                <img class="img-responsive" src="{{$article->article_cover->file_url}}" alt="{{$article->article_title}}封面">
-                            </a>
-                            <p class="text-center">
-                                <a href="{{'/home/article/'.$article->article_id}}">
-                                    {{$article->article_title}}
-                                </a>
-                            </p>
-                        </li>
-                    </ul>
-                </div>
-            @endforeach
-        </div>
-    </div>
-    <div id="hot_list" class="col-xs-6 hidden-xs">
-        <div id="hotArticle" class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title">最热文章</h3>
-            </div>
-            <div class="panel-body panel-body-hot">
-                <ul id="append_hot" class="list-unstyled">
-                    @foreach($articlesHot as $article)
-                        <li class="li-hot">
-                            <blockquote class="text-danger">
-                                <a href="{{'/home/article/'.$article->article_id}}" target="_blank">{{$article->article_title}}</a>
-                            </blockquote>
-                            <div class="img-out">
-                                <a href="{{'/home/article/'.$article->article_id}}" target="_blank">
-                                    <img class="img-responsive img-hot pull-left" src="{{$article->article_cover->file_url}}" alt="文章图片">
-                                </a>
-                            </div>
-                            <p class="article_content">
-                                <?=$article->article_summary?>
-                            </p>
-                        </li>
-                    @endforeach
+    <div class="col-xs-12 col-md-9">
+        <div class="col-xs-12">
+            <div class="alert alert-info">
+                <a href="#" class="close" data-dismiss="alert">
+                    &times;
+                </a>
+                <h3 class="text-danger">你想成为什么样的程序猿?</h3>
+                <ul>
+                    <li><span class="text-danger">90%的人属于三流公司招聘的程序员：</span>日复一日的写业务逻辑，修改bug，学当下最流行的框架，不断巩固实践，直到能够单独带领团队，成为所谓的全栈式开发。
+                       以为代码就是一切，产品就是一切，不断的摸爬滚打，若干年后达到这个层次的金字塔顶端，可是依然摆脱不了最底层的码农称谓。
+                       你所看到的不是全世界，其实是最底层。
+                    </li>
+                    <li>
+                       <span class="text-danger">7%的人是二流公司的架构师或者cto：</span>凡事都要刨根究底，拿到一个框架就要研究源代码，看看效率如何，怎么改更好。这种人往往积极性很高，逻辑思维强，往往是很多公司的技术总监，对每一行代码都要想想在机器上发生的事情。他们工作在it的前沿，最新资源，消息往往是最早知道的！
+                    </li>
+                    <li><span class="text-danger">2.9%的人是一流公司招聘的核心研发人员，国际标准化组织会员：</span>真正意义上的软件研发，从无到有的过程，他们往往是各大框架的制定者，各种标准的参与者。</li>
+                    <li><span class="text-danger">最后那0.1%的人更多的是天才，能够发明新东西的人：</span>c语言之父，图灵等等。</li>
                 </ul>
             </div>
         </div>
+        <div id="new_list" class="col-xs-12 col-sm-4">
+            <div id="append_new" class="panel panel-info">
+                <div class="panel-heading">
+                    <h3 class="panel-title">最新文章</h3>
+                </div>
+                @foreach($articlesNew as $article)
+                    <div class="panel-body padding-none">
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="{{'/home/article/'.$article->article_id}}">
+                                    <img class="img-responsive" src="{{$article->article_cover->file_url}}" alt="{{$article->article_title}}封面">
+                                </a>
+                                <p class="text-center">
+                                    <a href="{{'/home/article/'.$article->article_id}}">
+                                        {{$article->article_title}}
+                                    </a>
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+        <div id="hot_list" class="hidden-xs col-sm-8">
+            <div id="hotArticle" class="panel panel-info">
+                <div class="panel-heading">
+                    <h3 class="panel-title">最热文章</h3>
+                </div>
+                <div class="panel-body panel-body-hot">
+                    <ul id="append_hot" class="list-unstyled">
+                        @foreach($articlesHot as $article)
+                            <li class="li-hot">
+                                <blockquote class="text-danger">
+                                    <a href="{{'/home/article/'.$article->article_id}}" target="_blank">{{$article->article_title}}</a>
+                                </blockquote>
+                                <div class="img-out">
+                                    <a href="{{'/home/article/'.$article->article_id}}" target="_blank">
+                                        <img class="img-responsive img-hot pull-left" src="{{$article->article_cover->file_url}}" alt="文章图片">
+                                    </a>
+                                </div>
+                                <p class="article_content">
+                                    <?=$article->article_summary?>
+                                </p>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
-    <div id="other_list" class="col-xs-3 hidden-xs">
+    <div id="other_list" class="col-xs-12 col-md-3">
         <div class="panel panel-warning">
             <div class="panel-heading">
                 <h3 class="panel-title">我爱这个世界</h3>
@@ -127,7 +148,7 @@
                 </div>
             @endforeach
         </div>
-        <div class="panel panel-default">
+        <div class="panel panel-info">
             <div class="panel-heading">
                 <h3 class="panel-title">扫我! 扫我!</h3>
             </div>
@@ -135,16 +156,27 @@
                 <a href="/home/about/me" target="_blank"><img src="/img/home/index/weixin.jpg" class="img-responsive" alt="个人二维码"></a>
             </div>
             <div class="panel-footer">
-                <span class="text-info">闷骚的二维码颜色,做爱做的事情</span>
+                <span class="text-info">我有故事，你有酒吗？</span>
             </div>
         </div>
-        <div class="panel panel-default">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <h3 class="panel-title">思过崖在知乎</h3>
+            </div>
+            <div class="panel-body padding-none">
+                <a href="https://www.zhihu.com/people/siguoya" target="_blank"><img src="/img/home/index/zhihu.png" class="img-responsive" alt="思过崖在知乎"></a>
+            </div>
+            <div class="panel-footer">
+                <span class="text-info">关注编程技术与互联网产品。。。</span>
+            </div>
+        </div>
+        <div class="panel panel-info">
             <div class="panel-heading">
                 <h3 class="panel-title">友情链接</h3>
             </div>
             <div class="panel-body">
                 <ul class="list-unstyled">
-                    <li><a href="http://irudder.me/wpblog/">艾文逗</a></li>
+                    <li><a target="_blank" href="http://irudder.me/wpblog/">艾文逗</a></li>
                 </ul>
             </div>
         </div>
